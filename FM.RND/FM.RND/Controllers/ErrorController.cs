98df5778f9ad908f7735902604df872ba59ed875
csgct1004 <csgct1004@gmail.com>
2022-07-01 10:37:47 +0900

    Error 수정

--- FM.RND/FM.RND/Controllers/ErrorController.cs
+++ FM.RND/FM.RND/Controllers/ErrorController.cs
@@ -15,12 +15,19 @@ namespace FM.RND.Controllers
 
         public ActionResult Error404()
         {
+            Response.StatusCode = 404;
             return View();
         }
 
         public ActionResult Error500()
         {
+            Response.StatusCode = 500;
+
+            //View Return 방식
             return View();
+            //return Vie w("../Error/Error500");
+            //return View("./Error500");
+            //return View("/Views/Error/Error500.cshtml");
         }
     }
 }
